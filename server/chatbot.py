8298a97d9632472f2a3fd86ccced0304559b4b56
yuyounghyun <ryh2803@gmail.com>
2020-11-16 13:40:13 +0900

    server base

---
+++ server/chatbot.py
@@ -0,0 +1,55 @@
+rom flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/keyboard')
+def Keyboard():
+    dataSend = {
+      "user" : "corona_chatbot",
+      "Subject" : "OSSP",
+    }
+    return jsonify(dataSend)
+
+@app.route('/message', methods=['POST'])
+def Message():
+    content = request.get_json()
+    print(content)
+    content = content['userRequest']
+    content = content['utterance']
+
+    if content.rstrip() == "안녕":
+        dataSend = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "carousel": {
+                            "type" : "basicCard",
+                            "items": [
+                                {
+                                    "title" : "",
+                                    "description" : "서버테스트"
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        }
+    else :
+        dataSend = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "simpleText":{
+                            "text" : "질문을 이해하지 못했습니다."
+                        }
+                    }
+                ]
+            }
+        }
+    return jsonify(dataSend)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0') # default Flask port : 5000
---
+++ server/readme.md
@@ -0,0 +1,14 @@
+# Chatbot-Flask-Server
+
+The Flask server communicating with kakao Corona chatbot.
+
+
+### Package
+#### Flask
+```bash
+pip install Flask
+```
+
+### Test
+* example
+`http://ip-adress:5000/keyboard`
