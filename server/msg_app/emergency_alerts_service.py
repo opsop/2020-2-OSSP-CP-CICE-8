1788050dbda5a24e3c7a2673a73f7c4540bac39e
0hyunU <ryh2803@gmail.com>
2020-11-28 18:13:08 +0900

    filepath update using os package

--- server/msg_app/emergency_alerts_service.py
+++ server/msg_app/emergency_alerts_service.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 import json
 import sqlite3
-
+import os
 # try:
 #     conn.close()
 # except:
@@ -10,35 +10,37 @@ import sqlite3
 # SQLite DB 연결
 
 # Connection 으로부터 Cursor 생성
-disasterDBPath = './disaster_message_temp.db'
+disasterDBPath = os.path.dirname(__file__) + '/disaster_message_temp.db'
 # @app.route('/city_info', methods = ['POST'])
 def emergency_alerts(body):
-    conn = sqlite3.connect(disasterDBPath)
-    cur = conn.cursor()
-    # data = json.loads(body)
+    try:
+        #print(disasterDBPath)
+        conn = sqlite3.connect(disasterDBPath)
+        cur = conn.cursor()
+        # data = json.loads(body)
 
-    # req = request.get_json()
+        # req = request.get_json()
 
-    li = "%" + "%".join(body["sys_location"].split(" ")) + "%"
-    # li = "%"+"%".join(body["city"].split(" "))+"%"
+        li = "%" + "%".join(body["sys_location"].split(" ")) + "%"
+        # li = "%"+"%".join(body["city"].split(" "))+"%"
 
-    # SQL 쿼리 실행
-    cur.execute("select * from MESSAGE where location_name like '" + li + "'")
-    # 데이터 Fetch
-    rows = cur.fetchall()
-    msg_list = []
-    for row in rows:
-        msg_list.append(row)
-    # for row in rows:
-    #   print(row)
-    if len(msg_list) > 3:
-        msg_list = msg_list[0:3]
+        # SQL 쿼리 실행
+        cur.execute("select * from MESSAGE where location_name like '" + li + "'")
+        # 데이터 Fetch
+        rows = cur.fetchall()
+        msg_list = []
+        for row in rows:
+            msg_list.append(row)
+        # for row in rows:
+        #   print(row)
+        if len(msg_list) > 3:
+            msg_list = msg_list[0:3]
 
-    elif len(msg_list) == 0:
-        res = {"msg_list": "최근에 온 재난문자가 없습니다."}
-        return jsonify(res)
+        elif len(msg_list) == 0:
+            res = {"msg_list": "최근에 온 재난문자가 없습니다."}
+            return (res)
 
-    res = {"msg_list": msg_list}
-    return jsonify(res)
-
-    conn.close()
+        res = {"msg_list": msg_list}
+        return (res)
+    finally:
+        conn.close()
