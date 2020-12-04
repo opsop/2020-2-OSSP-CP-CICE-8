2fcf6b62c61579d446416a4eaa77d8be45ca8d40
i-zro <dek3127@gmail.com>
2020-12-04 23:46:26 +0900

    youtube_db

--- server/youtube.py
+++ server/youtube.py
@@ -1,37 +1,33 @@
-import requests
-import html
+import sqlite3
+import os
 
-def you_news(search_word):
-    search_word = search_word['action']['detailParams']
-    search_word = search_word['youtube']['value']
-    search_word = search_word+'뉴스'
-    youtube_key = "AIzaSyDgTuKL86WcF3uS6b6gBWEUoaUdd_cAOOA"
-    news_num = 5
-    r = requests.get(f"https://www.googleapis.com/youtube/v3/search?part=snippet&key={youtube_key}&q={search_word}19&maxResults={news_num}")
-    j = r.json()
-    item_list = []
-    if list(j.keys())[0] != "error":
-        for i in j['items']:
-            item_list.append({
-                                    "title": html.unescape(i['snippet']['title']),
-                                    "description": html.unescape(i['snippet']['description']),
-                                    "thumbnail": {
-                                        "imageUrl": i['snippet']['thumbnails']['high']['url']},
-                                    "buttons": [
-                                        {
-                                            "action": "webLink",
-                                            "label": "자세히 보러가기",
-                                            "webLinkUrl": f"https://www.youtube.com/watch?v={i['id']['videoId']}"
-                                        }
-                                    ]
-                                })
+item_list = []
 
-        send = {
-            "version": "2.0",
-            "template": {
-                "outputs": [
-                    {
-                        "carousel": {
-                            "type": "basicCard",
-                            "items": item_list}}]}}
+DB_PATH = os.path.dirname(__file__) + '/CoronaBotDB'
+
+def tube_get(param = '코로나 확진자'):
+    con = sqlite3.connect(DB_PATH+"/tube.db")
+    res = con.cursor().execute("""SELECT * from NEWS WHERE CATEGORY='%s' """ % (param)).fetchall()
+    for i in res:
+        item_list.append({
+            "title": i[1],
+            "description": i[2],
+            "thumbnail": {
+                "imageUrl": i[3]},
+            "buttons": [
+                {
+                    "action": "webLink",
+                    "label": "뉴스 보러가기",
+                    "webLinkUrl": i[4]
+                }
+            ]
+        })
+    send = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "carousel": {
+                        "type": "basicCard",
+                        "items": item_list}}]}}
     return send
---
+++ server/youtube_db.py
@@ -0,0 +1,49 @@
+import sqlite3
+import requests
+import html
+import os
+
+DB_PATH = os.path.dirname(__file__) + '/CoronaBotDB'
+# con = sqlite3.connect(DB_PATH + '/tube.db')
+# cursor = con.cursor()
+# cursor.execute("CREATE TABLE NEWS(CATEGORY text, TITLE text, DESC text, IMG text, LINK text)")
+
+def insert_db(data):
+    con = sqlite3.connect(DB_PATH + '/tube.db')
+    cursor = con.cursor()
+    for row in data:
+        cursor.execute("INSERT INTO NEWS VALUES(?, ?, ?, ?, ?)", (row[0], row[1], row[2], row[3], row[4]))
+        con.commit()
+    con.close()
+
+
+def refresh_db():
+    con = sqlite3.connect(DB_PATH+'/tube.db')
+    con.execute("DELETE FROM NEWS").rowcount
+    con.commit()
+    con.close()
+
+
+def crawl_tube():
+    youtube_key = "AIzaSyDgTuKL86WcF3uS6b6gBWEUoaUdd_cAOOA"
+    news_num = 5
+    add_list = [] # 저장할 아이템들
+    search_list = ['코로나 확진자', '코로나 백신', '코로나 후유증']
+
+    for i in search_list:
+        r = requests.get(
+            f"https://www.googleapis.com/youtube/v3/search?part=snippet&key={youtube_key}&q={i.replace(' ', '+') + '+뉴스'}19&maxResults={news_num}")
+        j = r.json()
+
+        for item in j['items']:
+            title = html.unescape(item['snippet']['title'])
+            desc = html.unescape(item['snippet']['description'])
+            r_img = item['snippet']['thumbnails']['high']['url']
+            j_img = f"https://www.youtube.com/watch?v={item['id']['videoId']}"
+
+            add_list.append([i, title, desc, r_img, j_img])
+
+    insert_db(add_list)
+
+refresh_db()
+crawl_tube()
