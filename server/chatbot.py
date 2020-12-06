ff3e326b8e96f9e1889b1ffa10ad5f6cc77b8ef5
권나영 <48379869+i-zro@users.noreply.github.com>
2020-12-06 15:27:41 +0900

    Update chatbot.py

--- server/chatbot.py
+++ server/chatbot.py
@@ -45,7 +45,7 @@ sched = BackgroundScheduler({'apscheduler.timezone': 'Asia/Seoul'})
 
 sched.add_job(update_db,'cron', day_of_week='0-6', hour=18)
 sched.add_job(news_update, 'interval', hours=2)
-sched.add_job(distance_update,'interval', hours = 24)
+sched.add_job(distance_update,'interval', hours = 3)
 sched.add_job(update_KoreaDB, 'cron', day_of_week='0-6', hour=14)
 sched.start()
 
