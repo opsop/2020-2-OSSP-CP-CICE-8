5c367fcd0d884db53e5b822ef278551467c303ee
i-zro <dek3127@gmail.com>
2020-12-09 08:38:30 +0900

    korea_graph

--- server/Sociallev.py
+++ server/Sociallev.py
@@ -93,6 +93,49 @@ def level(param):
             ]
           }
         }
+        answer = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "carousel": {
+                        "type": "basicCard",
+                        "items": [
+                            {
+                                "thumbnail": {
+                                    "imageUrl": "https://user-images.githubusercontent.com/48379869/101219822-eeef8b00-36c7-11eb-801b-950fc81992c5.png"
+                                },
+                                "buttons": [
+                                    {
+                                        "action": "webLink",
+                                        "label": "자가진단 하러가기",
+                                        "webLinkUrl": "http://aiselftest.com/covid/"},
+                                    {
+                                        "action": "share",
+                                        "label": "공유하기"}
+                                ]
+                            },
+                            {
+                                "title": "코로나 블루 자가진단",
+                                "description": "몸보다 마음이 더 힘들어요... 나도 혹시 코로나 블루?",
+                                "thumbnail": {
+                                    "imageUrl": "https://user-images.githubusercontent.com/48379869/101220311-d0d65a80-36c8-11eb-9048-65719ec7e408.png"},
+                                "buttons": [
+                                    {
+                                        "action": "webLink",
+                                        "label": "자가진단 하러가기",
+                                        "webLinkUrl": "https://trost.co.kr/promotion/channel/covid-19-with-depressive/"},
+                                    {
+                                        "action": "share",
+                                        "label": "공유하기"}
+                                ]
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+    }
         return answer
 
     else:
